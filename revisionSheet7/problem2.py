a4ae1a3fc59e85f50328f020048c67bbf0f55a1a
utkarshcs18 <utkarshkumar.cs18@gmail.com>
2026-07-28 02:16:47 +0530

    add solved problem

---
+++ revisionSheet7/problem2.py
@@ -0,0 +1,7 @@
+l = ["Sachin", "Rohan", "Atul" , "Ayush"]
+
+for name in l:
+    if name.startswith('S'):
+        print(f"Hey {name}! There Hru !")
+    else:
+        print(f"Hey {name}! There Hru without S !")
