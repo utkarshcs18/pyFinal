46bd1865b450d988a2e44761c00bd3e86871021f
utkarshcs18 <utkarshkumar.cs18@gmail.com>
2026-07-28 02:22:55 +0530

    add solved problem

---
+++ revisionSheet7/problem3.py
@@ -0,0 +1,18 @@
+a = int(input("Enter your Number: "))
+isPrime = True
+
+count = 0
+
+if(a == 0 or a == 1):
+    print("not prime")
+else:
+    for i in range (2, (a//2)+1):
+        if(a % i == 0):
+            isPrime = False
+            break;
+
+if(isPrime):
+    print("prime")
+else:
+    print("not prime")
+              
